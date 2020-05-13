e46024b7996bd202c826358ac9c4b206ecf82835
vparayaoan <parayaoanvanessa@gmail.com>
2020-05-13 14:18:24 -0400

    completed Section 2

---
+++ Section 2 - Simple Linear Regression/simple_linear_regression.py
@@ -0,0 +1,46 @@
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset 
+dataset = pd.read_csv('Salary_Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=0)
+
+# Training the Simple Linear Regression model on the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(x_test)
+
+# Visualising the Training set results
+plt.scatter(x_train, y_train, color = 'red')
+plt.plot(x_train, regressor.predict(x_train), color = 'blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Test set results
+plt.scatter(x_test, y_test, color = 'red')
+plt.plot(x_train, regressor.predict(x_train), color = 'blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#Making a single prediction for the salary of an employee with 12 years of experience
+print(regressor.predict([[12]]))
+
+#Retrieving the final linear regression equation coefficient values
+print(regressor.coef_)
+print(regressor.intercept_)
